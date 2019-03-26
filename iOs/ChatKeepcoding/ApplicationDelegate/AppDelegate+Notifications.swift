373c7054286c5bf7ebe41ef5022d6fd51c68876c
eric.risco <eric.risco@andornet.ad>
2019-03-27 00:37:32 +0100

    Dia 2

---
+++ iOs/ChatKeepcoding/ApplicationDelegate/AppDelegate+Notifications.swift
@@ -0,0 +1,9 @@
+//
+//  AppDelegate+Notifications.swift
+//  ChatKeepcoding
+//
+//  Created by Eric Risco de la Torre on 26/03/2019.
+//  Copyright © 2019 ERISCO. All rights reserved.
+//
+
+import Foundation
---
+++ iOs/Network/Managers/FirebaseImplemantation/LogFirebase.swift
@@ -0,0 +1,9 @@
+//
+//  LogFirebase.swift
+//  ChatKeepcoding
+//
+//  Created by Eric Risco de la Torre on 27/03/2019.
+//  Copyright © 2019 ERISCO. All rights reserved.
+//
+
+import Foundation

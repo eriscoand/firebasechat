034b1c0f4af57f638bb14ebbef0338fd981bb817
eric.risco <eric.risco@andornet.ad>
2019-03-26 20:31:30 +0100

    Dia 1

---
+++ iOs/Network/Managers/FirebaseImplemantation/MessageFirebase.swift
@@ -0,0 +1,59 @@
+//
+//  MessageFirebase.swift
+//  ChatKeepcoding
+//
+//  Created by Eric Risco de la Torre on 25/03/2019.
+//  Copyright © 2019 ERISCO. All rights reserved.
+//
+
+import Foundation
+import MessageKit
+import Firebase
+import FirebaseDatabase
+
+public class MessageFirebase: MessageManager {
+    
+    public var discussion: Discussion
+    var ref: DatabaseReference
+    
+    public required init(discussion: Discussion) {
+        self.discussion = discussion
+        self.ref = Database.database().reference().child("messages").child(self.discussion.uid)
+    }
+    
+    public func list(onSuccess: @escaping ([Message]) -> Void, onError: ErrorClosure?) {
+        
+        self.ref.observe(.value, with: { (snapshot) in
+            
+            let messages = snapshot.children
+                .compactMap({ Message.mapper(snapshot: $0 as! DataSnapshot) })
+                .sorted(by: { $0.sentDate < $1.sentDate })
+            
+            onSuccess(messages)
+            
+        }) { (error) in
+            if let retError = onError {
+                retError(error)
+            }
+        }
+        
+    }
+    
+    public func add(message: Message, onSuccess: @escaping () -> Void, onError: ErrorClosure?) {
+        
+        let child = Message.toDict(message: message)
+        
+        self.ref.child(message.messageId).updateChildValues(child) { (error, _ ) in
+            
+            if let err = error, let retError = onError {
+                retError(err)
+            }
+            
+            onSuccess()
+            
+        }
+        
+        
+    }
+    
+}
---
+++ iOs/Network/Managers/FirebaseImplemantation/Models/Message+Firebase.swift
@@ -0,0 +1,73 @@
+//
+//  Message+Firebase.swift
+//  ChatKeepcoding
+//
+//  Created by Eric Risco de la Torre on 25/03/2019.
+//  Copyright © 2019 ERISCO. All rights reserved.
+//
+
+import Foundation
+import MessageKit
+import Firebase
+import FirebaseDatabase
+
+extension Message {
+    
+    public class func mapper(snapshot: DataSnapshot) -> Message? {
+        
+        guard let json = snapshot.value as? [String:Any] else {
+            return nil
+        }
+        
+        let senderId = json["senderId"] as? String ?? ""
+        let displayName = json["displayName"] as? String ?? ""
+        let sender = Sender.init(id: senderId, displayName: displayName)
+        
+        let messageId = json["messageId"] as? String ?? ""
+        
+        let dateString = json["sentDate"] as? String ?? ""
+        let sentDate = Date.fromStringToDate(input: dateString, format: "yyyy-MM-dd HH:mm:ss")
+        
+        let type = json["type"] as? String ?? ""
+        let messageData: MessageData
+        
+        let value = json["value"] as? String ?? ""
+        
+        switch type {
+        case "text":
+            messageData = MessageData.text(value)
+            break
+        case "image":
+            let placeholder = UIImage.init(named: "placeholder")
+            messageData = MessageData.photo(placeholder!)
+            break
+        default:
+            messageData = MessageData.text(value)
+            break
+        }
+        
+        
+        let message = Message.init(sender: sender, messageId: messageId, sentDate: sentDate, data: messageData, type: type, value: value)
+        
+        return message
+        
+    }
+    
+    public class func toDict(message: Message) -> [String: String] {
+        
+        let date = Date.fromDateToString(date: Date(), format: "yyyy-MM-dd HH:mm:ss")
+        
+        var dict = [String: String]()
+        
+        dict["messageId"] = message.messageId
+        dict["senderId"] = message.sender.id
+        dict["displayName"] = message.sender.displayName
+        dict["sentDate"] = date
+        dict["type"] = message.type
+        dict["value"] = message.value
+        
+        return dict
+        
+    }
+    
+}
